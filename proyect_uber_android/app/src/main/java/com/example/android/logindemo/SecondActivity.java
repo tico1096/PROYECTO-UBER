5ea97586d5275ace7dd94ad4720ea9d8104f736d
Nicolas Tovar <nicolas.tovar@repremundo.com.co>
2020-05-14 18:43:46 -0500

    subir proyecto uber

---
+++ proyect_uber_android/app/src/main/java/com/example/android/logindemo/SecondActivity.java
@@ -0,0 +1,71 @@
+package com.example.android.logindemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private Button logout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        logout = (Button)findViewById(R.id.btnLogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Logout();
+            }
+        });
+    }
+
+    private void Logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(SecondActivity.this, MainActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.logoutMenu:{
+                Logout();
+                break;
+            }
+            case R.id.mapsMenu: {
+                startActivity(new Intent(SecondActivity.this, GMuber.class));
+                break;
+            }
+            case R.id.profileMenu: {
+                startActivity(new Intent(SecondActivity.this, ProfileActivity.class));
+                break;
+            }
+            case R.id.refreshMenu:
+                startActivity(new Intent(SecondActivity.this, Perfil_usuario.class));
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
